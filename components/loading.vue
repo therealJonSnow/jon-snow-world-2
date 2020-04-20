0988a77be96cb16cea138dbd1c315e917db65d8e
Jonny Snow <jonny@studiorepublic.com>
2020-04-20 13:35:56 +0100

    Theme Mobile

--- components/loading.vue
+++ components/loading.vue
@@ -1,8 +1,6 @@
 <template lang="html">
   <div class="loading-page" v-if="loading">
-
     <p>Loading...</p>
-    <div id="lottie"></div>
   </div>
 </template>
 
@@ -18,28 +16,11 @@ export default {
     finish () {
       this.loading = false
     }
-  },
-  mounted() {
-    var animation = bodymovin.loadAnimation({
-      container: document.getElementById('lottie'), // Required
-      path: '/data.json', // Required
-      renderer: 'svg/canvas/html', // Required
-      loop: true, // Optional
-      autoplay: true, // Optional
-      name: "Hello World", // Name for future reference. Optional.
-    })
   }
 }
 </script>
 
 <style scoped>
-.lottie {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-}
 .loading-page {
   position: fixed;
   top: 0;
